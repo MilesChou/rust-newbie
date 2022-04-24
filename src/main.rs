f51e2b96b49019015f0bea01e1f2112ab68e1fe6
MilesChou <jangconan@gmail.com>
2022-04-24 15:08:08 +0800

    Add Course 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let arg = env::args().nth(1).expect("no score given");
+
+    let score = arg.parse::<i32>().ok().expect("I wasn't given an integer!");
+
+    println!("{}", grade(score));
+}
+
+fn grade(score: i32) -> &'static str {
+    if score < 0 || score > 100 {
+        panic!("error range");
+    }
+
+    if score >= 90 {
+        return "考試優秀"
+    }
+
+    if score >= 80 {
+        return "考試良好"
+    }
+
+    if score >= 60 {
+        return "考試及格"
+    }
+
+    "下次繼續努力"
 }
